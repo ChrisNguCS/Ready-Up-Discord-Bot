e438236a461266f38e4e4481299f2d30152cc544
nchris128 <nchris128@gmail.com>
2022-08-31 20:56:26 -0400

    Delete tester.py

--- tester.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-db = sqlite3.connect('main.sqlite')
-cursor = db.cursor()
-playerQuery = cursor.execute(f"SELECT user_id FROM queue").fetchall()
-playerList = ""
-for row in playerQuery:
-        playerList += "".join(row) + "\n"
-print(playerList)
